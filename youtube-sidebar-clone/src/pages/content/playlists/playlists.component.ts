32eba089381bfabd2965ec71419eebaf32c17b64
snehalrenushe <snehalrenushe15@gmail.com>
2024-06-20 18:51:24 +0530

    youtube sidebar clone added

---
+++ youtube-sidebar-clone/src/pages/content/playlists/playlists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlists',
+  standalone: true,
+  imports: [],
+  templateUrl: './playlists.component.html',
+  styleUrl: './playlists.component.css'
+})
+export class PlaylistsComponent {
+
+}
